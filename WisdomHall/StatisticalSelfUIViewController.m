1fc33dfb060f3367115a9ff19e125c029d347573
majixning <1013258870@qq.com>
2018-05-05 18:23:03 +0800

    统计接口调通

--- WisdomHall/StatisticalSelfUIViewController.m
+++ WisdomHall/StatisticalSelfUIViewController.m
@@ -19,6 +19,17 @@
 @property (nonatomic,strong) NSArray * ary;
 @property (nonatomic,assign) int temp;
 @property (nonatomic,strong) NSMutableArray * dataAry;
+
+@property (nonatomic,assign)int yesr;
+@property (nonatomic,assign)int moth;
+@property (nonatomic,assign)int day;
+
+@property (nonatomic,assign) int  pickTemp;
+
+@property (nonatomic,copy)NSString * startTime;
+
+@property (nonatomic,copy)NSString * endTime;
+
 @end
 
 @implementation StatisticalSelfUIViewController
@@ -28,7 +39,17 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     _ary = @[@"2017-8至2018-3学期",@"2018-3至2018-8学期",@"2018-8至2019-3学期",@"2019-3至2019-8学期",@"2019-8至2020-3学期"];
+    
     _temp = 0;
+    
+    _pickTemp = 1;
+    
+    _yesr = 0;
+    
+    _moth = 0;
+    
+    _day = 0;
+    
     _dataAry = [NSMutableArray arrayWithCapacity:1];
     
     [self addTableView];
@@ -38,8 +59,11 @@
     self.navigationController.navigationBarHidden = NO; //设置隐藏
 }
 -(void)getDataWith:(NSString *)t{
+    
     UserModel * user = [[Appsetting sharedInstance] getUsetInfo];
-    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:[NSString stringWithFormat:@"%@",user.peopleId],@"userId",t,@"termId", nil];
+    
+    NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:[NSString stringWithFormat:@"%@",user.peopleId],@"userId",t,@"termId",_startTime,@"startTime",_endTime,@"endTime", nil];
+    
     [[NetworkRequest sharedInstance] GET: StatisticsSelf dict:dict succeed:^(id data) {
         NSString * str = [[data objectForKey:@"header"] objectForKey:@"code"];
         if (![UIUtils isBlankString:str]) {
@@ -61,6 +85,7 @@
     }];
 }
 - (IBAction)querySelf:(UIButton *)sender {
+    
     [self addPickView];
 }
 -(void)addTableView{
@@ -107,21 +132,93 @@
     [self.view addSubview:self.pickerView];
 }
 -(void)outView{
+    
     [self.bView removeFromSuperview];
     [self.pickerView removeFromSuperview];
     self.pickerView = nil;
+    
 }
 -(void)leftButton{
+    
     [self.bView removeFromSuperview];
     [self.pickerView removeFromSuperview];
     
+    _temp = 0;
+    _yesr = 0;
+    _moth = 0;
+    _day = 0;
 }
 -(void)rightButton{
+    
     [self.bView removeFromSuperview];
+    
     [self.pickerView removeFromSuperview];
-    [self getDataWith:[NSString stringWithFormat:@"%d",_temp+1]];
-    [_selectT setTitle:_ary[_temp] forState:UIControlStateNormal];
-    _temp = 0;
+    
+    _bView = nil;
+    
+    _pickerView = nil;
+    
+    if (_pickTemp == 1) {
+        
+        [_selectT setTitle:_ary[_temp] forState:UIControlStateNormal];
+        
+        _pickTemp = 2;
+        
+        [self addPickView];
+        
+    }else if (_pickTemp == 2){
+        if (_moth<9) {
+            _startTime = [NSString stringWithFormat:@"%d-0%d",2017+_yesr,_moth];
+            if (_day<9) {
+                _startTime = [_startTime stringByAppendingString:[NSString stringWithFormat:@"-0%d",_day]];
+            }else{
+                _startTime = [_startTime stringByAppendingString:[NSString stringWithFormat:@"-%d",_day]];
+            }
+        }else{
+            _startTime = [NSString stringWithFormat:@"%d-%d",2017+_yesr,_moth+1];
+            if (_day<9) {
+                _startTime = [_startTime stringByAppendingString:[NSString stringWithFormat:@"-0%d",_day+1]];
+            }else{
+                _startTime = [_startTime stringByAppendingString:[NSString stringWithFormat:@"-%d",_day+1]];
+            }
+        }
+        _yesr = 0;
+        _moth = 0;
+        _day = 0;
+        
+        _pickTemp = 3;
+        [self addPickView];
+    }else if(_pickTemp == 3){
+        if (_moth<9) {
+            _endTime = [NSString stringWithFormat:@"%d-0%d",2017+_yesr,_moth+1];
+            if (_day<9) {
+                _endTime = [_endTime stringByAppendingString:[NSString stringWithFormat:@"-0%d",_day+1]];
+            }else{
+                _endTime = [_endTime stringByAppendingString:[NSString stringWithFormat:@"-%d",_day+1]];
+            }
+        }else{
+            _endTime = [NSString stringWithFormat:@"%d-%d",2017+_yesr,_moth+1];
+            if (_day<9) {
+                _endTime = [_endTime stringByAppendingString:[NSString stringWithFormat:@"-0%d",_day+1]];
+            }else{
+                _endTime = [_endTime stringByAppendingString:[NSString stringWithFormat:@"-%d",_day+1]];
+            }
+        }
+        _yesr = 0;
+        
+        _moth = 0;
+        
+        _day = 0;
+        
+        _pickTemp = 1;
+        
+        [self getDataWith:[NSString stringWithFormat:@"%d",_temp+1]];
+        
+        _temp = 0;
+
+        
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -131,25 +228,58 @@
 #pragma mark UIPickViewDelegate
 
 -(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-    
-    return 1;
+    if (_pickTemp == 1) {
+        return 1;
+    }else{
+        return 3;
+    }
 }
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    
-    return _ary.count;
+    if (_pickTemp == 1) {
+        return _ary.count;
+    }else{
+        if (component == 0) {
+            return 3;
+        }else if(component == 1){
+            return 12;
+        }else if (component ==2){
+            return 31;
+        }
+    }
+    return 0;
 }
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
-    if (row<_ary.count) {
-        return _ary[row];
+    if (_pickTemp == 1) {
+        if (row<_ary.count) {
+            return _ary[row];
+        }
+    }else{
+        if (component == 0) {
+            return [NSString stringWithFormat:@"%ld",2017+row];
+        }else if(component == 1){
+            return [NSString stringWithFormat:@"%ld",1+row];
+        }else if (component ==2){
+            return [NSString stringWithFormat:@"%ld",1+row];
+        }
     }
+    
     return @"";
 }
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
-   
-    _temp = (int)row;
+    if (_pickTemp == 1) {
+        _temp = (int)row;
+    }else{
+        if (component == 0) {
+            _yesr = (int)row;
+        }else if(component == 1){
+            _moth = (int)row;
+        }else if (component ==2){
+            _day = (int)row;
+        }
+    }
 }
 #pragma mark UITableViewdelegate
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
